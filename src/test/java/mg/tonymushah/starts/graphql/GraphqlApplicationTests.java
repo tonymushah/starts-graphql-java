b3f6c9368d9743fab5f4b956d20fd85d02f6d358
tonymushah <tonyramanantsalama@gmail.com>
2023-09-27 15:45:41 +0300

    Added spring boot starters

---
+++ src/test/java/mg/tonymushah/starts/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.starts.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
